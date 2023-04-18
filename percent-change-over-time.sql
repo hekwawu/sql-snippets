f5e32c84fff11415a48af431944e076f67d30675
hekwawu <130410090+hekwawu@users.noreply.github.com>
2023-04-17 20:35:18 -0400

    percent-change-over-time

---
+++ percent-change-over-time.sql
@@ -0,0 +1,34 @@
+-- This script is great for calculating any type of QoQ, MoM, WoW etc change
+-- Could easily have metrics added to make a whole table of trended metrics
+
+-- Table schema is as follows (no code provided):
+	id INT
+	created_at DATE
+	value INT
+	purchase_id INT
+	
+-- Calculate totals by month
+WITH month_total AS (
+SELECT
+    to_char(created_at,'YYYY-MM') as year_month, 
+    sum(value) AS revenue
+FROM sf_transactions
+group by year_month
+order by year_month),
+
+-- Create view with lagged data view 
+prev_month AS (
+SELECT    
+    year_month,
+    revenue as curr_rev,
+    LAG(revenue) OVER (ORDER BY year_month) AS prev_rev
+FROM month_total)
+
+-- Perform caalculations
+SELECT
+    year_month,
+    round(
+        ((curr_rev - prev_rev)::numeric / prev_rev::numeric)
+        *100,2) AS revenue_diff_pct
+FROM prev_month;
+    
